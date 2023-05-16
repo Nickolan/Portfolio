9cea27cd9658fbdd57e8da1449447180f4d4e085
Nicolas <nicolassantiagonavarrete.nsn@gmail.com>
2023-05-16 16:34:23 -0300

    Cv & Certificates Link

--- carrusel.js
+++ carrusel.js
@@ -1,9 +1,14 @@
 const images = ['./certificates/henryCertificate.JPG', './certificates/EFSET_Certificate.JPG'];
+const certificates = [
+  {image: './certificates/henryCertificate.JPG', link:'https://certificates.soyhenry.com/new-cert?id=32fcafea6c28a1df17ef7984d43235e80183be429a9d618508b4bae40a3788cd'}, 
+  {image: './certificates/EFSET_Certificate.JPG', link: 'https://www.efset.org/cert/DpiorT'}
+]
 let currentIndex = 0;
 
 const nextButton = document.getElementById('next-button');
 const prevButton = document.getElementById('prev-button');
 const image = document.getElementById('carousel-image');
+const Certi = document.getElementById('0');
 
 nextButton.addEventListener('click', handleNext);
 prevButton.addEventListener('click', handlePrev);
@@ -21,5 +26,6 @@ function handlePrev() {
 }
 
 function updateImage(imageElement) {
-  imageElement.src = images[currentIndex];
+  imageElement.src = certificates[currentIndex].image;
+  Certi.href = certificates[currentIndex].link;
 }
--- tscripts.ts
+++ tscripts.ts
@@ -1,8 +1,10 @@
 
 const CvButton = document.getElementById('CV');
+const Cert = document.getElementById('0');
+const EnCert = document.getElementById('1');
 
 if (CvButton) {
   CvButton.addEventListener('click', function() {
-    window.open('./files/Nicolas-Navarrete-CV-EN.pdf');
+    window.open('https://drive.google.com/file/d/1cXCIOxFKW42xn7fI9DVHwUymG7sfhdJi/view?usp=sharing');
   });
 }
