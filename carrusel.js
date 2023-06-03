43a15cfd0d9fdb9d9e1e02d023f48006470963b6
Nicolas <nicolassantiagonavarrete.nsn@gmail.com>
2023-06-03 15:19:03 -0300

    temp version styles

--- carrusel.js
+++ carrusel.js
@@ -29,3 +29,38 @@ function updateImage(imageElement) {
   imageElement.src = certificates[currentIndex].image;
   Certi.href = certificates[currentIndex].link;
 }
+
+//                PROYECTS CARROUSEL
+
+// const imagesProject = ['./files/Proyects/FindYourPerrito-Title.JPG', './files/Proyects/BOOKS4ALL.png', './files/Proyects/study-with-pomo.JPG' ];
+// const projects = [
+//   {image: './files/Proyects/FindYourPerrito-Title.JPG', link:'https://find-your-perrito-client.vercel.app'}, 
+//   {image: './files/Proyects/BOOKS4ALL.png', link: 'https://books4-all-front.vercel.app'},
+//   {image: './files/Proyects/study-with-pomo.JPG', link: 'https://study-with-pomo.vercel.app'}
+// ]
+// let projectIndex = 0;
+
+// const next = document.getElementById('next-button-Project');
+// const prev = document.getElementById('prev-button-Project');
+// const project = document.getElementById('carousel-image-Project');
+// const proj = document.getElementById('0');
+
+// next.addEventListener('click', handleNext);
+// prev.addEventListener('click', handlePrev);
+
+// updateImage(project);
+
+// function handleNext() {
+//   projectIndex = (projectIndex + 1) % imagesProject.length;
+//   updateImage(project);
+// }
+
+// function handlePrev() {
+//   projectIndex = (projectIndex - 1 + imagesProject.length) % imagesProject.length;
+//   updateImage(project);
+// }
+
+// function updateImage(imageElement) {
+//   imageElement.src = projects[projectIndex].image;
+//   proj.href = projects[projectIndex].link;
+// }
