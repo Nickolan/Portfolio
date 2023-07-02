966c3caaf9ef2fb6625991fa765cc42400b54dbd
Nicolas <nicolassantiagonavarrete.nsn@gmail.com>
2023-07-02 11:53:37 -0300

    mobile incomplete

--- contactScript.js
+++
@@ -1,8 +0,0 @@
-const copyButton = document.getElementById('copy'); 
-var input = document.getElementById('email');
-
-copyButton?.addEventListener("click", function(event) {
-    event.preventDefault();
-    input?.select();
-    document.execCommand("copy");
-});
